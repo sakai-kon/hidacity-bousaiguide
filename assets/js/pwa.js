4c6c8acd0c9c44a1c6cd9f50246ea87e4be71c40
sakai <314073247+sakai-kon@users.noreply.github.com>
2026-08-20 20:52:56 +0900

    Bump Service Worker version for icon update

--- assets/js/pwa.js
+++ assets/js/pwa.js
@@ -3,7 +3,7 @@
  * Service Worker登録 + 全ページ共通UIスタイルのロード。
  */
 
-const SW_VERSION = "2026-08-20-9";
+const SW_VERSION = "2026-08-20-10";
 const UI_VERSION = "2026-08-20-ui2";
 const SEARCH_STYLE_VERSION = "2026-08-20-search2";
 const BASE = "/hidacity-bousaiguide";
