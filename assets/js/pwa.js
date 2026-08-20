ca8162def92cd8415b2b9faf0df4a4e6f82c85a7
sakai <314073247+sakai-kon@users.noreply.github.com>
2026-08-20 18:40:27 +0900

    Force Service Worker refresh after site-wide theme update

--- assets/js/pwa.js
+++ assets/js/pwa.js
@@ -1,13 +1,20 @@
 /**
  * pwa.js
  * Service Worker(sw.js)の登録処理のみを行う。
+ * サイト更新時に古いService Workerが残らないよう、明示的に更新を促す。
  */
+
+const SW_VERSION = "2026-08-20-2";
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("/hidacity-bousaiguide/sw.js", { scope: "/hidacity-bousaiguide/" })
+      .register(`/hidacity-bousaiguide/sw.js?v=${SW_VERSION}`, {
+        scope: "/hidacity-bousaiguide/",
+        updateViaCache: "none",
+      })
       .then((reg) => {
+        reg.update().catch(() => {});
         console.log("[pwa] Service Worker registered:", reg.scope);
       })
       .catch((err) => {
